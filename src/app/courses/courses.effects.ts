7ffc0a1dc60be55e8a772c0c36b7a0e1f17c6d76
André <andre.escocard@hesea.com.br>
2023-03-06 13:54:14 -0300

    feat: save data on background with effect

--- src/app/courses/courses.effects.ts
+++ src/app/courses/courses.effects.ts
@@ -19,6 +19,18 @@ export class CoursesEffects{
             )
     );
 
+    saveCourses$ = createEffect(
+        () => this.actions$
+        .pipe(
+            ofType(CourseActions.courseUpdated),
+            concatMap(action => this.coursesHttpService.saveCourse(
+                action.update.id,
+                action.update.changes
+            ))
+        ),
+        {dispatch: false}
+    );
+
     constructor(private actions$: Actions, private coursesHttpService: CoursesHttpService){
         
     }
